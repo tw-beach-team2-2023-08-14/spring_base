d17741fe4494010dd7625a808274f4d64ac44ce2
SONGYI-chen123 <song1577660501@163.com>
2022-12-11 12:03:23 +0800

    [yichen] init integration test.

---
+++ src/integrationTest/java/com/example/BaseIntegrationTest.java
@@ -0,0 +1,20 @@
+package com.example;
+
+import io.restassured.RestAssured;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public abstract class BaseIntegrationTest {
+  @LocalServerPort
+  int port;
+
+  @Before
+  public void setUp() {
+    RestAssured.port = port;
+  }
+}
