df037758b7abfe40218be132bd9eee71fb0b5e2a
junyiguo <junyi.guo@thoughtworks.com>
2023-08-10 17:49:22 +0800

    [TL-14][Junyi] change the input param type from string to uuid when retrieve orders

--- src/integrationTest/java/com/example/OrderControllerIntegrationTest.java
+++ src/integrationTest/java/com/example/OrderControllerIntegrationTest.java
@@ -5,6 +5,7 @@ import static org.hamcrest.Matchers.equalTo;
 import static org.springframework.http.HttpStatus.OK;
 
 import com.github.database.rider.core.api.dataset.DataSet;
+import java.util.UUID;
 import org.junit.jupiter.api.Test;
 
 public class OrderControllerIntegrationTest extends BaseIntegrationTest {
@@ -12,13 +13,14 @@ public class OrderControllerIntegrationTest extends BaseIntegrationTest {
   @Test
   @DataSet("retrieve_orders_on_order_table.yml")
   public void should_retrieve_order_list_by_customer_id_successfully() {
+    UUID customerId = UUID.fromString("dcabcfac-6b08-47cd-883a-76c5dc366d88");
     given()
         .when()
-        .get("/orders/{customer_id}", "abcd")
+        .get("/orders/{customer_id}", customerId)
         .then()
         .statusCode(OK.value())
         .body("[0].id", equalTo(1))
-        .body("[0].customerId", equalTo("abcd"))
+        .body("[0].customerId", equalTo(customerId.toString()))
         .body("[0].totalPrice", equalTo(20.0F))
         .body("[0].status", equalTo("CREATED"))
         .body("[0].productDetails[0].name", equalTo("water"));
--- src/main/java/com/example/presentation/facade/OrderController.java
+++ src/main/java/com/example/presentation/facade/OrderController.java
@@ -3,6 +3,7 @@ package com.example.presentation.facade;
 import com.example.application.service.OrderApplicationService;
 import com.example.presentation.vo.OrderListDto;
 import java.util.List;
+import java.util.UUID;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,7 +17,7 @@ public class OrderController {
   private final OrderApplicationService orderApplicationService;
 
   @GetMapping("/{customer_id}")
-  public List<OrderListDto> retrieveOrderListByCustomerId(@PathVariable String customer_id) {
-    return orderApplicationService.findByCustomerId(customer_id);
+  public List<OrderListDto> retrieveOrderListByCustomerId(@PathVariable UUID customer_id) {
+    return orderApplicationService.findByCustomerId(customer_id.toString());
   }
 }
