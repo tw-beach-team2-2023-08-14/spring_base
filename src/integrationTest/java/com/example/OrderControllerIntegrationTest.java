db98be28867880d6f4a1a843b957403945f00e61
JIN Mengting <mting2645@gmail.com>
2023-08-11 04:59:48 +0800

    [TL-13][Mengting] implement integration test for create order api

--- src/integrationTest/java/com/example/OrderControllerIntegrationTest.java
+++ src/integrationTest/java/com/example/OrderControllerIntegrationTest.java
@@ -5,6 +5,13 @@ import static org.hamcrest.Matchers.equalTo;
 import static org.springframework.http.HttpStatus.OK;
 
 import com.github.database.rider.core.api.dataset.DataSet;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import java.util.List;
+import java.util.Map;
+import net.minidev.json.JSONArray;
+import net.minidev.json.JSONObject;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class OrderControllerIntegrationTest extends BaseIntegrationTest {
@@ -50,5 +57,31 @@ public class OrderControllerIntegrationTest extends BaseIntegrationTest {
         .body("[0].createTime", equalTo("2023-08-10T12:35:13"))
         .body("[0].productDetails[0].name", equalTo("water"))
         .body("size()", equalTo(1));
+  }
+
+  @Test
+  @DataSet("save_order_successfully.yml")
+  public void should_save_order_and_return_order_id_successfully() {
+
+    JSONObject orderProductOne = new JSONObject();
+    orderProductOne.putAll(Map.of("productId", 1001, "quantity", 1L));
+    JSONObject orderProductTwo = new JSONObject();
+    orderProductTwo.putAll(Map.of("productId", 1002, "quantity", 2L));
+    JSONObject orderProductThree = new JSONObject();
+    orderProductThree.putAll(Map.of("productId", 1003, "quantity", 3L));
+
+    JSONArray orderProducts = new JSONArray();
+    orderProducts.addAll(List.of(orderProductOne, orderProductTwo, orderProductThree));
+
+    JSONObject orderRequest = new JSONObject();
+    orderRequest.putAll(
+        Map.of(
+            "customerId", "dcabcfac-6b08-47cd-883a-76c5dc366d88", "orderProducts", orderProducts));
+    String orderReqBody = orderRequest.toJSONString();
+
+    Response response =
+        given().contentType(ContentType.JSON).body(orderReqBody).when().post("/orders/creation");
+    Assertions.assertEquals(OK.value(), response.statusCode());
+    Assertions.assertNotNull(response.body());
   }
 }
