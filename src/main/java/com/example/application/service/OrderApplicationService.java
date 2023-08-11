0701de1a494c5b79ea7f148e3533c5a82ba7c734
JIN Mengting <mting2645@gmail.com>
2023-08-11 17:29:37 +0800

    [TL-20][Mengting] add pre-push hook in .git directory by adding new copy task in gradle

--- src/main/java/com/example/application/service/OrderApplicationService.java
+++ src/main/java/com/example/application/service/OrderApplicationService.java
@@ -1,5 +1,9 @@
 package com.example.application.service;
 
+import static com.example.application.assembler.OrderListDtoMapper.MAPPER;
+import static com.example.common.exception.BaseExceptionCode.INVALID_PRODUCT;
+import static com.example.domain.entity.OrderStatus.CREATED;
+
 import com.example.common.exception.BusinessException;
 import com.example.domain.convertor.ProductConvertor;
 import com.example.domain.entity.Order;
@@ -13,64 +17,60 @@ import com.example.presentation.vo.OrderProductReqDto;
 import com.example.presentation.vo.OrderReqDto;
 import com.example.presentation.vo.ProductStatus;
 import com.fasterxml.jackson.core.JsonProcessingException;
-import lombok.AllArgsConstructor;
-import org.springframework.stereotype.Service;
-
 import java.math.BigDecimal;
 import java.util.List;
 import java.util.function.Function;
-
-import static com.example.application.assembler.OrderListDtoMapper.MAPPER;
-import static com.example.common.exception.BaseExceptionCode.INVALID_PRODUCT;
-import static com.example.domain.entity.OrderStatus.CREATED;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
 
 @Service
 @AllArgsConstructor
 public class OrderApplicationService {
-    private final ProductRepository productRepository;
-    private final OrderRepository orderRepository;
+  private final ProductRepository productRepository;
+  private final OrderRepository orderRepository;
 
-    public List<OrderListDto> findOrderByCustomerIdAndOrderId(String customerId, String orderId) {
-        return orderRepository.findByCustomerId(customerId).stream()
-                .map(MAPPER::toDto)
-                .filter(orderListDto -> orderId == null || orderId.equals(orderListDto.getOrderId()))
-                .toList();
+  public List<OrderListDto> findOrderByCustomerIdAndOrderId(String customerId, String orderId) {
+    return orderRepository.findByCustomerId(customerId).stream()
+        .map(MAPPER::toDto)
+        .filter(orderListDto -> orderId == null || orderId.equals(orderListDto.getOrderId()))
+        .toList();
+  }
+
+  public String createOrder(OrderReqDto orderReqDto) throws JsonProcessingException {
+
+    List<ProductDetail> productDetails = getProductDetails(orderReqDto);
+
+    BigDecimal totalPrice = OrderUtil.calculateTotalPrice(productDetails);
+
+    Order order =
+        Order.builder()
+            .customerId(orderReqDto.getCustomerId())
+            .totalPrice(totalPrice)
+            .status(CREATED)
+            .productDetails(productDetails)
+            .build();
+
+    return orderRepository.save(order);
+  }
+
+  private List<ProductDetail> getProductDetails(OrderReqDto orderReqDto) {
+
+    Function<OrderProductReqDto, ProductDetail> toProductDetail =
+        (orderProductReqDto) -> {
+          Product product = productRepository.findById(orderProductReqDto.getProductId());
+          checkValidStatus(product);
+          ProductDetail productDetail = ProductConvertor.toProductDetail(product);
+          productDetail.setAmount(orderProductReqDto.getQuantity());
+          return productDetail;
+        };
+
+    return orderReqDto.getOrderProducts().stream().map(toProductDetail).toList();
+  }
+
+  private static void checkValidStatus(Product product) {
+    if (product.getStatus() == ProductStatus.INVALID) {
+      throw new BusinessException(
+          INVALID_PRODUCT, "Product of id [" + product.getId() + "] is invalid");
     }
-
-    public String createOrder(OrderReqDto orderReqDto) throws JsonProcessingException {
-
-        List<ProductDetail> productDetails = getProductDetails(orderReqDto);
-
-        BigDecimal totalPrice = OrderUtil.calculateTotalPrice(productDetails);
-
-        Order order = Order.builder()
-                .customerId(orderReqDto.getCustomerId())
-                .totalPrice(totalPrice)
-                .status(CREATED)
-                .productDetails(productDetails)
-                .build();
-
-        return orderRepository.save(order);
-    }
-
-    private List<ProductDetail> getProductDetails(OrderReqDto orderReqDto) {
-
-        Function<OrderProductReqDto, ProductDetail> toProductDetail =
-                (orderProductReqDto) -> {
-                    Product product = productRepository.findById(orderProductReqDto.getProductId());
-                    checkValidStatus(product);
-                    ProductDetail productDetail = ProductConvertor.toProductDetail(product);
-                    productDetail.setAmount(orderProductReqDto.getQuantity());
-                    return productDetail;
-                };
-
-        return orderReqDto.getOrderProducts().stream().map(toProductDetail).toList();
-    }
-
-    private static void checkValidStatus(Product product) {
-        if (product.getStatus() == ProductStatus.INVALID) {
-            throw new BusinessException(
-                    INVALID_PRODUCT, "Product of id [" + product.getId() + "] is invalid");
-        }
-    }
+  }
 }
