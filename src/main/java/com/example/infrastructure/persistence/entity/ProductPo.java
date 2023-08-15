f9e58625d193de03b55d5199cd984238fb858223
Linhao Jia <252620356@qq.com>
2023-08-15 11:31:25 +0800

    [TL-0201][Linhao] spotless fmt

--- src/main/java/com/example/infrastructure/persistence/entity/ProductPo.java
+++ src/main/java/com/example/infrastructure/persistence/entity/ProductPo.java
@@ -4,11 +4,7 @@ import static jakarta.persistence.GenerationType.IDENTITY;
 
 import jakarta.persistence.*;
 import java.math.BigDecimal;
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Data
 @Entity
@@ -17,6 +13,7 @@ public class ProductPo {
   @Id
   @GeneratedValue(strategy = IDENTITY)
   private Integer id;
+
   private String name;
   private BigDecimal price;
   private String status;
