d29757a941e73be0aa9160f6cf02b7e16c664126
JIN Mengting <mengting.jin@thoughtworks.com>
2023-08-15 17:56:45 +0800

    [0206][Zishu & Mengting] move calculate total price unit test from order util test to order factory test

--- src/test/groovy/com/example/domain/factory/OrderFactoryTest.groovy
+++ src/test/groovy/com/example/domain/factory/OrderFactoryTest.groovy
@@ -1,29 +1,27 @@
-package com.example.domain.util
-
+package com.example.domain.factory
 
 import com.example.domain.entity.ProductDetail
-import com.example.presentation.vo.OrderProductReqDto
 import org.assertj.core.api.Assertions
 import spock.lang.Specification
 
-class OrderUtilTest extends Specification {
+class OrderFactoryTest extends Specification {
+
     def "should calculate total price correctly"() {
         given:
         Integer PRODUCT_ID_ONE = 1
         Integer PRODUCT_ID_TWO = 2
         Long QUANTITY = 10L
-        List<OrderProductReqDto> productDetails = List.of(
+        List<ProductDetail> productDetails = List.of(
                 new ProductDetail(PRODUCT_ID_ONE, "testProductOne", BigDecimal.TEN, QUANTITY),
                 new ProductDetail(PRODUCT_ID_TWO, "testProductTwo", BigDecimal.TEN, QUANTITY)
         )
 
 
         when:
-        BigDecimal result = OrderUtil.calculateTotalPrice(productDetails)
+        BigDecimal result = OrderFactory.calculateTotalPrice(productDetails)
 
         then:
         Assertions.assertThat(result)
                 .isEqualTo(BigDecimal.valueOf(200))
     }
-
 }
