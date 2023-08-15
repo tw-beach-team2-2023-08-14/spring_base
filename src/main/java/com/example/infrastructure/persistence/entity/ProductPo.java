d62dd2cd39e1029c7767f64a2f61845284dc2a14
Linhao Jia <252620356@qq.com>
2023-08-15 11:13:28 +0800

    [TL-0201][Linhao] change productPo to meet new db schema

--- src/main/java/com/example/infrastructure/persistence/entity/ProductPo.java
+++ src/main/java/com/example/infrastructure/persistence/entity/ProductPo.java
@@ -5,24 +5,22 @@ import static jakarta.persistence.GenerationType.IDENTITY;
 import jakarta.persistence.*;
 import java.math.BigDecimal;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
+@Data
 @Entity
 @Table(name = "product")
 public class ProductPo {
   @Id
   @GeneratedValue(strategy = IDENTITY)
   private Integer id;
-
   private String name;
-
   private BigDecimal price;
-
   private String status;
+  private BigDecimal discount;
+  private BigDecimal salePrice;
+  private Integer inventory;
 }
